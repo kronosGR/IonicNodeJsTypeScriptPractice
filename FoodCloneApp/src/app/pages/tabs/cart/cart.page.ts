178f9232b39736b7cbe6192a956c03e3fe6073bb
kronosGR <geo.elgeo@gmail.com>
2022-11-26 20:21:13 +0100

    pass the data to the cart

--- FoodCloneApp/src/app/pages/tabs/cart/cart.page.ts
+++ FoodCloneApp/src/app/pages/tabs/cart/cart.page.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from "@angular/router";
+import {Preferences} from "@capacitor/preferences";
 
 @Component({
   selector: 'app-cart',
@@ -6,10 +8,68 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cart.page.scss'],
 })
 export class CartPage implements OnInit {
+  urlCheck: any;
+  url: any;
+  model: any = {};
+  deliveryCharge = 20;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private router: Router) {
   }
 
+  ngOnInit() {
+    this.checkUrl();
+    this.getCartData();
+  }
+
+  checkUrl() {
+    let url: any = this.router.url.split('/');
+    const spliced = url.splice(url.length - 2, 2);
+    this.urlCheck = spliced[0]
+    url.push(this.urlCheck);
+    this.url = url;
+  }
+
+  getPreviousUrl() {
+    return this.url.join('/');
+  }
+
+  getCard() {
+    return Preferences.get({key: 'cart'});
+  }
+
+  async getCartData() {
+    let data: any = await this.getCard();
+    if (data?.value) {
+      this.model = await JSON.parse(data.value);
+      this.calculate();
+    }
+  }
+
+  async calculate() {
+    let item = this.model.items.filter((x: any) => x.quantity > 0);
+    this.model.items = item;
+    this.model.totalPrice = 0;
+    this.model.totalItem = 0;
+    this.model.deliveryCharge = 0;
+    this.model.grandTotal = 0;
+    item.forEach((element: any) => {
+      this.model.totalItem += element.quantity;
+      this.model.totalPrice += (parseFloat(element.price) * parseFloat(element.quantity));
+    });
+    this.model.deliveryCharge = this.deliveryCharge;
+    this.model.totalPrice = parseFloat(this.model.totalPrice).toFixed(2);
+    this.model.grandTotal = (parseFloat(this.model.totalPrice) + parseFloat(this.model.deliveryCharge)).toFixed(2);
+    if (this.model.totalItem == 0) {
+      this.model.totalItem = 0;
+      this.model.totalPrice = 0;
+      this.model.grandTotal = 0;
+      await this.clearCart();
+      this.model = null;
+    }
+    console.log('cart: ', this.model);
+  }
+
+  clearCart() {
+    Preferences.remove({key: 'cart'});
+  }
 }
--- FoodCloneApp/src/app/pages/tabs/items/items.page.ts
+++ FoodCloneApp/src/app/pages/tabs/items/items.page.ts
@@ -246,7 +246,7 @@ export class ItemsPage implements OnInit {
 
   async viewCart() {
     if(this.cartData.items && this.cartData.items.length > 0) await this.saveToCart();
-    // this.router.navigate([this.router.url + '/cart']);
+    this.router.navigate([this.router.url + '/cart']);
   }
 
 }
