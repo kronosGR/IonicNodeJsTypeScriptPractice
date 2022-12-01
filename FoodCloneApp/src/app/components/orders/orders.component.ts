883d218a51755f5b1ef60d1280a5e1a0a23117fd
kronosGR <geo.elgeo@gmail.com>
2022-12-01 20:22:38 +0100

    refactoring and added part functionality  to account tab page

---
+++ FoodCloneApp/src/app/components/orders/orders.component.ts
@@ -0,0 +1,24 @@
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+
+@Component({
+  selector: 'app-orders',
+  templateUrl: './orders.component.html',
+  styleUrls: ['./orders.component.scss'],
+})
+export class OrdersComponent implements OnInit {
+  @Input() order: any;
+  @Output() reorder: EventEmitter<any> = new EventEmitter<any>();
+  @Output() help: EventEmitter<any>=new EventEmitter<any>();
+
+  constructor() { }
+
+  ngOnInit() {}
+
+  reorderItem() {
+    this.reorder.emit(this.order);
+  }
+
+  getHelp() {
+    this.help.emit(this.order);
+  }
+}
