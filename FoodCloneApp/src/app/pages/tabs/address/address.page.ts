6e81ef8b20742321395cca9b57d65179eb73c639
kronosGR <geo.elgeo@gmail.com>
2022-12-02 06:03:39 +0100

    added addresses design

---
+++ FoodCloneApp/src/app/pages/tabs/address/address.page.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-address',
+  templateUrl: './address.page.html',
+  styleUrls: ['./address.page.scss'],
+})
+export class AddressPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  editAddress() {
+
+  }
+
+  deleteAddress() {
+
+  }
+}
