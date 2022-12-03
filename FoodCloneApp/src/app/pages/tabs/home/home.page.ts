b13d897d9f9badba5975f2b6f3a5296695511d72
kronosGR <geo.elgeo@gmail.com>
2022-12-03 07:00:52 +0100

    added api service and refactored the restaurants

--- FoodCloneApp/src/app/pages/tabs/home/home.page.ts
+++ FoodCloneApp/src/app/pages/tabs/home/home.page.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import SwiperCore, {Keyboard, Pagination, Scrollbar, SwiperOptions} from "swiper";
+import {ApiService} from "../../../services/api/api.service";
 
 
 @Component({
@@ -13,65 +14,14 @@ export class HomePage implements OnInit {
   isLoading: boolean = false;
 
 
-  constructor() {
+  constructor(private api: ApiService) {
   }
 
   ngOnInit() {
     this.isLoading = true;
     setTimeout(() => {
-      this.banners = [
-        {banner: 'assets/imgs/1.jpg'},
-        {banner: 'assets/imgs/2.jpg'},
-        {banner: 'assets/imgs/3.jpg'},
-      ]
-
-      this.restaurants = [
-        {
-          uid: '12wefdss',
-          cover: 'assets/imgs/1.jpg',
-          name: 'Stayfit',
-          short_name: 'stayfit',
-          address: 'Karol Bagh, New Delhi',
-          cuisines: [
-            'Italian',
-            'Mexican'
-          ],
-          rating: 5,
-          delivery_time: 25,
-          distance: 2.5,
-          price: 100
-        },
-        {
-          uid: '12wefdefsdss',
-          cover: 'assets/imgs/2.jpg',
-          name: 'Stayfit1',
-          short_name: 'stayfit1',
-          cuisines: [
-            'Italian',
-            'Mexican'
-          ],
-          rating: 5,
-          delivery_time: 25,
-          address: 'Karol Bagh, New Delhi',
-          distance: 2.5,
-          price: 100
-        },
-        {
-          uid: '12wefdssrete',
-          cover: 'assets/imgs/3.jpg',
-          name: 'Stayfit2',
-          short_name: 'stayfit2',
-          cuisines: [
-            'Italian',
-            'Mexican'
-          ],
-          rating: 5,
-          delivery_time: 25,
-          address: 'Karol Bagh, New Delhi',
-          distance: 2.5,
-          price: 100
-        },
-      ];
+      this.banners = this.api.banners;
+      this.restaurants = this.api.restaurants;
       this.isLoading = false;
     }, 1000);
 
---
+++ FoodCloneApp/src/app/services/api/api.service.ts
@@ -0,0 +1,61 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  banners = [
+    {banner: 'assets/imgs/1.jpg'},
+    {banner: 'assets/imgs/2.jpg'},
+    {banner: 'assets/imgs/3.jpg'},
+  ]
+
+  restaurants = [
+    {
+      uid: '12wefdss',
+      cover: 'assets/imgs/1.jpg',
+      name: 'Stayfit',
+      short_name: 'stayfit',
+      address: 'Karol Bagh, New Delhi',
+      cuisines: [
+        'Italian',
+        'Mexican'
+      ],
+      rating: 5,
+      delivery_time: 25,
+      distance: 2.5,
+      price: 100
+    },
+    {
+      uid: '12wefdefsdss',
+      cover: 'assets/imgs/2.jpg',
+      name: 'Stayfit1',
+      short_name: 'stayfit1',
+      cuisines: [
+        'Italian',
+        'Mexican'
+      ],
+      rating: 5,
+      delivery_time: 25,
+      address: 'Karol Bagh, New Delhi',
+      distance: 2.5,
+      price: 100
+    },
+    {
+      uid: '12wefdssrete',
+      cover: 'assets/imgs/3.jpg',
+      name: 'Stayfit2',
+      short_name: 'stayfit2',
+      cuisines: [
+        'Italian',
+        'Mexican'
+      ],
+      rating: 5,
+      delivery_time: 25,
+      address: 'Karol Bagh, New Delhi',
+      distance: 2.5,
+      price: 100
+    },
+  ];
+  constructor() { }
+}
