6717935853b6f87619deed7442e22dfdc48891d9
kronosGR <geo.elgeo@gmail.com>
2022-12-03 07:36:24 +0100

    added global service with toast and loader

--- FoodCloneApp/src/app/services/global/gloabl.service.ts
+++ FoodCloneApp/src/app/services/global/gloabl.service.ts
@@ -5,6 +5,7 @@ import {AlertController, LoadingController, ModalController, ToastController} fr
   providedIn: 'root'
 })
 export class GloablService {
+  isLoading: boolean = false;
 
   constructor(private alertCtrl: AlertController,
               private toastCtrl: ToastController,
@@ -12,10 +13,67 @@ export class GloablService {
               private modalCtrl: ModalController) {
   }
 
-  showAlert(){
-    this.alertCtrl.create({
-      header:,
-      message:
+  async showAlert(message: any, header?: any, buttonArray?: any[]) {
+    await this.alertCtrl.create({
+      header: header ? header : 'Authentication failed',
+      message: message,
+      buttons: buttonArray ? buttonArray : ['Okay']
+    })
+      .then(alertEl => alertEl.present);
+  }
+
+  async showToast(msg: any, color: any, position: any, duration = 3000) {
+    const toast = await this.toastCtrl.create({
+      message: msg,
+      duration: duration,
+      color: color,
+      position: position
     })
   }
+
+  errorToast(msg?: any, duration = 4000) {
+    this.showToast(
+      msg ? msg : 'No internet connection',
+      'danger',
+      'bottopm',
+      duration
+    )
+  }
+
+  successToast(msg: any) {
+    this.showToast(
+      msg,
+      'success',
+      'bottopm'
+    )
+  }
+
+  showLoader(msg?: any, spinner?: any) {
+    this.isLoading = true;
+    return this.loadingCtrl.create({
+      message: msg,
+      spinner: spinner ? spinner : 'bubbles'
+    }).then(res => {
+      res.present().then(() => {
+        if (!this.isLoading) {
+          res.dismiss().then(() => {
+            console.log('abort presenting')
+          })
+        }
+      })
+    })
+      .catch(e => {
+        console.log('error', e)
+      })
+  }
+
+  hideLoader() {
+    this.isLoading = false;
+    return this.loadingCtrl.dismiss()
+      .then(() => console.log('dismissed'))
+      .catch(e => console.log('error: ', e))
+  }
+
+  
+
 }
